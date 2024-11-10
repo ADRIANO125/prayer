5b9e007a96474e0b1bd0893f97cb81d1dd22b1ec
ADRIANO125 <abdulrahmankadri12@gmail.com>
2024-11-10 21:35:23 +0200

    Updates

---
+++ precache-manifest.e98b0260db7105f84a31deddfa123893.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3520513d89195da7def1f6b5f725143",
+    "url": "/prayer/index.html"
+  },
+  {
+    "revision": "2107e9de7da0d66335e8",
+    "url": "/prayer/static/css/main.a67832b0.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/prayer/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "/prayer/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "2107e9de7da0d66335e8",
+    "url": "/prayer/static/js/main.b2a1f02c.chunk.js"
+  },
+  {
+    "revision": "18a8f7bb1093da90fb23",
+    "url": "/prayer/static/js/runtime~main.c67e8d29.js"
+  },
+  {
+    "revision": "8ad1bef89042bb21e19550322fa14d19",
+    "url": "/prayer/static/media/bg.8ad1bef8.jpg"
+  }
+]);
